c2d7f2654eee60161e242db8d2359eb8bab2b9d0
nikhil <nikhillrbhat@gmail.com>
2022-10-27 00:07:57 +0530

    s

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ df=pd.read_csv('search.csv')
 salt="#j@nu$w&"
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://stgculhhzfqsnp:7b2285e352bee51479a4bc6dd01073794c5cc3270b18cb7dbfcf80b2427683ed@ec2-3-220-207-90.compute-1.amazonaws.com:5432/d96v1v718pbl7i"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://stgculhhzfqsnp:7b2285e352bee51479a4bc6dd01073794c5cc3270b18cb7dbfcf80b2427683ed@ec2-3-220-207-90.compute-1.amazonaws.com:5432/d96v1v718pbl7i"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 DATABASE_URL="postgres://stgculhhzfqsnp:7b2285e352bee51479a4bc6dd01073794c5cc3270b18cb7dbfcf80b2427683ed@ec2-3-220-207-90.compute-1.amazonaws.com:5432/d96v1v718pbl7i"
 db = SQLAlchemy(app)
